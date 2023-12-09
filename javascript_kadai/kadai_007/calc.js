ba31f20d7f13bbc06e092897ddcafbeacd05893f
matsushita16130 <gp16a130@oecu.jp>
2023-12-09 10:26:27 +0900

    JavaScript課題7の提出

---
+++ javascript_kadai/kadai_007/calc.js
@@ -0,0 +1,16 @@
+// 変数numの宣言
+let num = 1;
+
+// 分岐
+if (num % 3 == 0 && num % 5=== 0) {
+    console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+else if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+else {
+    console.log(num);
+}
